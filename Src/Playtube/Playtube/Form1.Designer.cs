5e4741525e0e02e7c0614cbf9dc1589229e27b57
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-10-14 08:59:48 +0200

    Fix a bug

--- Src/Playtube/Playtube/Form1.Designer.cs
+++ Src/Playtube/Playtube/Form1.Designer.cs
@@ -124,6 +124,7 @@
             this.Text = "Playtube";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
